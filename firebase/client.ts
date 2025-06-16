e425cd157de0590a3507932c0f9dd1cfd54e16b6
raymondPoonReciteMe <ray.poon@reciteme.com>
2025-06-16 18:11:33 +0100

    firebase setup

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC64ecpPIhaSuxSuYbn8iSFQr8R_OPCfbI",
+  authDomain: "prepwise-18086.firebaseapp.com",
+  projectId: "prepwise-18086",
+  storageBucket: "prepwise-18086.firebasestorage.app",
+  messagingSenderId: "128725645561",
+  appId: "1:128725645561:web:16733d187a980d35fd8a28",
+  measurementId: "G-BL2ESXMHL8",
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
